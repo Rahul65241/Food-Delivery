cbd5bb7b85fa1bccc1832ca01d744e98f14e38bc
Rahul <rahulkr11886@gmail.com>
2023-10-17 21:45:33 +0530

    sending user data on login

--- login.php
+++ login.php
@@ -4,10 +4,14 @@ require 'connect.php';
 $username=$_POST['username'];
 $password=$_POST['password'];
 
-$checkUser="SELECT * from user_register WHERE username='$username' and password='$password' ";
+$checkUser="SELECT name,username,contact,email from user_register WHERE username='$username' and password='$password' ";
 $checkQuery=mysqli_query($conn,$checkUser);
 
 if(mysqli_num_rows($checkQuery)>0){
+
+    while($row=$checkQuery->fetch_assoc()){
+        $response['data']=$row;
+    }
 
     $response['status_code']="1";
     $response['message']="Login Successfull";
